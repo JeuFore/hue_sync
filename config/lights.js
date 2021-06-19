009444ad26396f66e5ff14a52b00171287463acc
JeuFore <mat.chast67@hotmail.fr>
2021-06-19 19:35:51 +0200

    Add new light

--- config/lights.js
+++ config/lights.js
@@ -54,5 +54,33 @@ module.exports = {
         },
         "uniqueid": "00:17:88:01:00:bc:c7:b9-0b",
         "swversion": "5.105.0.21169"
+    },
+    "3": {
+        "state": {
+            "on": true,
+            "xy": [
+                0.5,
+                0.5
+            ],
+            "colormode": "xy",
+            "mode": "homeautomation",
+            "reachable": true
+        },
+        "swupdate": {
+            "state": "noupdates",
+            "lastinstall": "2018-01-02T19:24:20"
+        },
+        "type": "Router",
+        "name": "Hue play 3",
+        "modelid": "915005733701",
+        "manufacturername": "Philips",
+        "productname": "Hue Play",
+        "config": {
+            "archetype": "sultanbulb",
+            "function": "mixed",
+            "direction": "omnidirectional"
+        },
+        "uniqueid": "00:17:88:01:00:bc:c7:b9-0a",
+        "swversion": "5.105.0.21169"
     }
 };
